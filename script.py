2fd613f900c140241d81a585cf1ed28b14d8d898
unknown <sarah.godard@hotmail.fr>
2021-10-21 11:31:37 +0200

    feat (end basic tasks): new name OK, need to fix deprecated

--- script.py
+++ script.py
@@ -26,11 +26,17 @@ if __name__ == "__main__":
     login_button = browser.find_element_by_tag_name("input")
     login_button.submit()
     wait = WebDriverWait(browser, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
-    element = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Account')]")))
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Account')]")))
     account_button = browser.find_element(By.XPATH, "//a[contains(text(),'Account')]")
     account_button.click()
-
-
+    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, "//html[@id='html']/body/div/div/div[2]/div/div/div/form/div/input"))).click()
+    namebox = browser.find_element(By.XPATH, "//html[@id='html']/body/div/div/div[2]/div/div/div/form/div/input")
+    namebox.clear()
+    namebox.send_keys(os.getenv('PANDASCORE_NEWNAME'))
+    retype_password = browser.find_element(By.XPATH, "//html[@id='html']/body/div/div/div[2]/div/div/div/form/div[7]/input")
+    retype_password.send_keys(os.getenv('PANDASCORE_PASSWORD'))
+    sendform = browser.find_element(By.XPATH, "//html[@id='html']/body/div/div/div[2]/div/div/div/form/div[8]/button")
+    sendform.click()
 
     while True:
        c = 1
