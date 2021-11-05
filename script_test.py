d0c941c733e0dc27c8d5a3d93eb2889d9c75fb9e
unknown <sarah.godard@hotmail.fr>
2021-11-05 11:50:36 +0100

    test (script_test.py): test for the process

--- script_test.py
+++ script_test.py
@@ -4,8 +4,10 @@ import script
 
 if __name__ == "__main__":
     script.load_dotenv()
-    script.handling_error()
+    assert script.handling_error() == 0, "error handling not finalised"
     browser = script.init_brower()
-    script.login(browser)
-    script.go_to_account(browser)
-    script.change_name_from_account(browser)
+    assert browser != None, "browser not initialised"
+    assert script.login(browser) == 0, "login crashed"
+    assert script.go_to_account(browser) == 0, "account page not found"
+    names = script.change_name_from_account(browser)
+    assert names[0] != names[1], "name did not change"
